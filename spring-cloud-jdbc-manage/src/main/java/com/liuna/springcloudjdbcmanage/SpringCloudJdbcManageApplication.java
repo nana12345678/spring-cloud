a66353da348caac987b483fc1d828b3fa788a45f
liuna <na.liu@ximalaya.com>
2021-07-13 10:42:53 +0800

    spring-cloud

---
+++ spring-cloud-jdbc-manage/src/main/java/com/liuna/springcloudjdbcmanage/SpringCloudJdbcManageApplication.java
@@ -0,0 +1,13 @@
+package com.liuna.springcloudjdbcmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudJdbcManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudJdbcManageApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-jdbc-manage/src/test/java/com/liuna/springcloudjdbcmanage/SpringCloudJdbcManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liuna.springcloudjdbcmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudJdbcManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
