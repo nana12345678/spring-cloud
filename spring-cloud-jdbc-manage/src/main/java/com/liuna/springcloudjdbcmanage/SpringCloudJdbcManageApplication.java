0153fcaebe3aed933a2c4d105ac8064f87935885
naliu <user@naliu.local>
2021-09-27 20:56:25 +0800

    demand_update二次修改  请为您的变更输入提交说明。以 '#' 开始的行将被忽略，而一个空的提交

--- spring-cloud-jdbc-manage/src/main/java/com/liuna/springcloudjdbcmanage/SpringCloudJdbcManageApplication.java
+++ spring-cloud-jdbc-manage/src/main/java/com/liuna/springcloudjdbcmanage/SpringCloudJdbcManageApplication.java
@@ -8,6 +8,9 @@ public class SpringCloudJdbcManageApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudJdbcManageApplication.class, args);
+        /*这是我demand_update分支的修改*/
+        System.out.println("这是我demand_update分支的修改");
+
     }
 
 }
