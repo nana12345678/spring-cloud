5938acc21537c547788f91846bd0ae81500af6d5
nana12345678 <876651409@qq.com>
2021-10-20 17:26:02 +0800

    合并前面的commit

--- spring-cloud-jdbc-manage/src/main/java/com/liuna/springcloudjdbcmanage/SpringCloudJdbcManageApplication.java
+++ spring-cloud-jdbc-manage/src/main/java/com/liuna/springcloudjdbcmanage/SpringCloudJdbcManageApplication.java
@@ -10,6 +10,22 @@ public class SpringCloudJdbcManageApplication {
         SpringApplication.run(SpringCloudJdbcManageApplication.class, args);
         System.out.println("这是demand_insert的修改内容");
         System.out.println("这是insert的第二次修改");
+
+        System.out.println("feature/hamei_test_demand这是insert的第三次修改");
+
+        System.out.println("hamei_update_1 commit 第一次");
+        System.out.println("hamei_update_1 commit 第二次");
+
+        System.out.println("hamei_update_1 commit 第三次");
+
+        System.out.println("hamei_update_1 commit 第四次");
+
+        System.out.println("hamei_update_1 commit 第5⃣五次");
+
+        System.out.println("hamei_update_1 commit 第六次");
+
+
+        System.out.println("hamei_update_1 commit 第七次");
     }
 
 }
