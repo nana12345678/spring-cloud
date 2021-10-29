d70ed3c1519e2e9d2e9610d54d50dbd5a5b3cd64
nana12345678 <876651409@qq.com>
2021-10-29 08:40:21 +0800

    第wu次修改

--- spring-cloud-jdbc-manage/src/main/java/com/liuna/springcloudjdbcmanage/SpringCloudJdbcManageApplication.java
+++ spring-cloud-jdbc-manage/src/main/java/com/liuna/springcloudjdbcmanage/SpringCloudJdbcManageApplication.java
@@ -25,6 +25,11 @@ public class SpringCloudJdbcManageApplication {
         System.out.println("feature/hamei_modify_two_fenzhi修改");
         System.out.println("feature/hamei_modify_two_fenzhi修改");
         System.out.println("feature/hamei_modify_two_fenzhi修改");
+
+        System.out.println("(feature/hamei_modify_five_fenzhi)修改");
+        System.out.println("(feature/hamei_modify_five_fenzhi)修改");
+        System.out.println("(feature/hamei_modify_five_fenzhi)修改");
+        System.out.println("(feature/hamei_modify_five_fenzhi)修改");
     }
 
 }
